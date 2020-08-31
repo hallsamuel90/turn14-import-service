49d399022bf43268c8c22f9b8b712838348e7640
vagrant <vagrant@vagrant.vm>
2020-08-31 01:36:24 +0000

    temp for testing

--- src/productMgmt/services/productMgmtService.ts
+++ src/productMgmt/services/productMgmtService.ts
@@ -128,9 +128,11 @@ export class ProductMgmtService {
       if (this.storeCarriesStock(turn14Product)) {
         const wcUpdateInventoryDto = wcMapper.turn14ToWc(turn14Product, wcId);
 
-        if (this.stockHasChanged(wcUpdateInventoryDto, wcProduct)) {
-          wcProducts.update.push(wcUpdateInventoryDto);
-        }
+        // if (this.stockHasChanged(wcUpdateInventoryDto, wcProduct)) {
+        //   wcProducts.update.push(wcUpdateInventoryDto);
+        // }
+
+        wcProducts.update.push(wcUpdateInventoryDto);
 
         await this.pushFullBatchOfWcProducts(wcProducts, wcRestApi);
       }
