c6fa24bf5f80700329c0f9182d5084b81aa5b4fb
vagrant <hallsamuel90@gmail.com>
2020-10-26 19:27:13 +0000

    BugFix for exceding rate limit: * Added axios-rate-limit dependency * Refactored Turn14RestApi, Turn14RestApiProvider

--- src/productMgmt/services/turn14Client.ts
+++ src/productMgmt/services/turn14Client.ts
@@ -1,8 +1,10 @@
+import _, { Dictionary } from 'lodash';
 import { Service } from 'typedi';
 import { Keys } from '../../apiUsers/models/apiUser';
 import { Turn14RestApi } from '../../turn14/clients/turn14RestApi';
 import { Turn14RestApiProvider } from '../../turn14/clients/turn14RestApiProvider';
 import { Turn14ProductDTO } from '../../turn14/dtos/turn14ProductDto';
+import { Turn14Brand } from '../../turn14/interfaces/turn14Brand';
 
 /**
  * Turn14Client for retrieving data from Turn14.
@@ -16,27 +18,76 @@ export class Turn14Client {
   }
 
   /**
+   * Fetch the brands from the turn14 api.
+   *
+   * @param {Keys} turn14Keys the api keys needed to access Turn14.
+   * @returns {Turn14Brand[]} a list of turn14 brands.
+   */
+  public async getBrands(turn14Keys: Keys): Promise<Turn14Brand[]> {
+    const turn14RestApi: Turn14RestApi = await this.turn14RestApiProvider.getTurn14RestApi(
+      turn14Keys.client,
+      turn14Keys.secret
+    );
+
+    return ((await turn14RestApi.fetchBrands()) as unknown) as Turn14Brand[];
+  }
+
+  /**
    * Gets all Turn14 Products for a given brand.
    *
    * @param {Keys} turn14Keys the api keys needed to access Turn14.
    * @param {string} brandId the id of the brand to retrieve products.
    * @returns {Promise<Turn14ProductDTO[]>} a list of Turn14 products.
    */
-  public async getTurn14ProductsByBrand(
+  public async getProductsByBrand(
     turn14Keys: Keys,
     brandId: string
   ): Promise<Turn14ProductDTO[]> {
-    const turn14RestApi: Turn14RestApi = this.turn14RestApiProvider.getTurn14RestApi(
+    const turn14RestApi: Turn14RestApi = await this.turn14RestApiProvider.getTurn14RestApi(
       turn14Keys.client,
       turn14Keys.secret
     );
 
-    await turn14RestApi.authenticate();
+    return await this.getAllBrandProductData(turn14RestApi, Number(brandId));
+  }
 
-    const turn14Products = await turn14RestApi.fetchAllBrandData(
-      Number(brandId)
-    );
+  private async getAllBrandProductData(
+    turn14RestApi: Turn14RestApi,
+    brandId: number
+  ): Promise<Turn14ProductDTO[]> {
+    const items = await turn14RestApi.fetchAllBrandItems(brandId);
+    const itemsSorted = this.sortDataById(items);
 
-    return turn14Products;
+    const itemData = await turn14RestApi.fetchAllBrandItemsData(brandId);
+    const itemDataById = this.mapDataById(itemData);
+
+    const itemPricing = await turn14RestApi.fetchAllBrandPricing(brandId);
+    const itemPricingById = this.mapDataById(itemPricing);
+
+    const itemInventory = await turn14RestApi.fetchAllBrandInventory(brandId);
+    const itemInventoryById = this.mapDataById(itemInventory);
+
+    const turn14ProductDtos: Turn14ProductDTO[] = [];
+    for (const item of itemsSorted) {
+      const itemId = item['id'];
+      const turn14ProductDto = new Turn14ProductDTO(
+        item,
+        itemDataById[itemId],
+        itemPricingById[itemId],
+        itemInventoryById[itemId]
+      );
+
+      turn14ProductDtos.push(turn14ProductDto);
+    }
+
+    return turn14ProductDtos;
+  }
+
+  private sortDataById(turn14ProductData: JSON[]): JSON[] {
+    return _.sortBy(turn14ProductData, 'id');
+  }
+
+  private mapDataById(jsonData: JSON[]): Dictionary<JSON> {
+    return _.keyBy(jsonData, 'id');
   }
 }
--- src/turn14/clients/turn14RestApi.ts
+++ src/turn14/clients/turn14RestApi.ts
@@ -1,13 +1,6 @@
-/* eslint-disable @typescript-eslint/camelcase */
-import axios, { AxiosInstance } from 'axios';
-import _, { Dictionary } from 'lodash';
-import { Turn14ProductDTO } from '../dtos/turn14ProductDto';
+import { AxiosResponse } from 'axios';
+import { RateLimitedAxiosInstance } from 'axios-rate-limit';
 import { Turn14Error } from '../errors/Turn14Error';
-import { Turn14Brand } from '../interfaces/turn14Brand';
-
-const BASE_URL = 'https://apitest.turn14.com/v1';
-const INVALID_CREDENTIALS =
-  '🔥 ERROR: Token expired or invalid, attempting to authenticate!';
 
 /**
  * Turn14RestApi.
@@ -17,330 +10,130 @@ const INVALID_CREDENTIALS =
  * @author Sam Hall <hallsamuel90@gmail.com>
  */
 export class Turn14RestApi {
-  turn14Client: string;
-  turn14Secret: string;
-  axiosClient: AxiosInstance;
+  private readonly axiosClient: RateLimitedAxiosInstance;
 
-  /**
-   * Constructs a new Turn14RestApi client
-   *
-   * @param {string} turn14Client the client key for access to the api.
-   * @param {string} turn14Secret the secret key for access to the api.
-   */
-  constructor(turn14Client: string, turn14Secret: string) {
-    this.turn14Client = turn14Client;
-    this.turn14Secret = turn14Secret;
-    this.axiosClient = axios.create({
-      baseURL: BASE_URL,
-    });
+  constructor(axiosClient: RateLimitedAxiosInstance) {
+    this.axiosClient = axiosClient;
   }
 
   /**
-   * Authenticates the Turn14API and attaches the auth token to
-   * all subsequent requests
+   * Fetches all brands.
    *
+   * @returns {Promise<JSON[]>} of brands.
    */
-  async authenticate(): Promise<void> {
-    const TOKEN_RESOURCE = '/token';
-    try {
-      const response = await this.axiosClient.post(TOKEN_RESOURCE, {
-        grant_type: 'client_credentials',
-        client_id: this.turn14Client,
-        client_secret: this.turn14Secret,
-      });
-      if (response.status == 200) {
-        console.info('🔑 Authenticated Turn14 API!');
-        const token = response.data.access_token;
-        this.axiosClient.defaults.headers.common = {
-          Authorization: `Bearer ${token}`,
-        };
-      }
-    } catch (e) {
-      console.error('🔥 ' + e);
-    }
+  public async fetchBrands(): Promise<JSON[]> {
+    const BRANDS_RESOURCE = 'brands';
+
+    return await this.getRequest(BRANDS_RESOURCE);
   }
 
   /**
-   * Fetches all brand data.
+   * Fetches all items for a given brand.
    *
-   * @param {number} brandId the unique brand id to fetch data for.
-   * @returns {Promise<Turn14ProductDTO[]>} a list of products containing all items, data, pricing,
-   * and inventory for a brand.
+   * @param {number} brandId the id of the brand to retrieve items for.
+   * @returns {Promise<JSON[]>} of brand items.
    */
-  async fetchAllBrandData(brandId: number): Promise<Turn14ProductDTO[]> {
-    const items = await this.fetchAllBrandItems(brandId);
-    const itemData = await this.fetchAllBrandItemsData(brandId);
-    const itemPricing = await this.fetchAllBrandPricing(brandId);
-    const itemInventory = await this.fetchAllBrandInventory(brandId);
-    const turn14ProductDtos: Turn14ProductDTO[] = [];
-    for (const item of items) {
-      const itemId = item['id'];
-      const turn14ProductDto = new Turn14ProductDTO(
-        item,
-        itemData[itemId],
-        itemPricing[itemId],
-        itemInventory[itemId]
-      );
-      turn14ProductDtos.push(turn14ProductDto);
-    }
-    return turn14ProductDtos;
-  }
-
-  /**
-   * Fetches all brand items sorted
-   *
-   * @param {number} brandId
-   * @returns {Promise<JSON[]>} of brand items
-   */
-  async fetchAllBrandItems(brandId: number): Promise<JSON[]> {
-    let allData: JSON[] = [];
-    let i = 1;
-    while (true) {
-      const pageData = await this.fetchBrandItems(brandId, i);
-      if (!Array.isArray(pageData) || !pageData.length) {
-        break;
-      }
-      allData = allData.concat(pageData);
-      i++;
-    }
-    return _.sortBy(allData, 'id');
-  }
-
-  /**
-   * Fetches brand items
-   *
-   * @param {number} brandId
-   * @param {number} pageNumber
-   * @returns {Promise<JSON>}
-   */
-  async fetchBrandItems(brandId: number, pageNumber: number): Promise<JSON> {
+  public async fetchAllBrandItems(brandId: number): Promise<JSON[]> {
     const BRAND_ITEMS_RESOURCE = `items/brand/${brandId}`;
-    try {
-      const response = await this.axiosClient.get(BRAND_ITEMS_RESOURCE, {
-        params: {
-          page: pageNumber,
-        },
-      });
-      return response.data.data;
-    } catch (e) {
-      if (e.response.status != undefined) {
-        if (e.response.status == 401) {
-          console.error(INVALID_CREDENTIALS);
-          await this.authenticate();
-          await this.fetchBrandItems(brandId, pageNumber);
-        }
-      } else {
-        console.error('🔥 ' + e);
 
-        throw new Turn14Error(
-          'fetchBrandItems(), something went wrong communicating with Turn14.'
-        );
-      }
-    }
-
-    return {} as JSON;
+    return await this.getAllPagedData(BRAND_ITEMS_RESOURCE);
   }
 
   /**
-   * Fetches all brand items data as a map of
-   * {'itemId', itemData}
+   * Fetches all item data for a given brand.
    *
-   * @param {number} brandId
-   * @returns {Promise<Dictionary<JSON>>} of brand items data
+   * @param {number} brandId the id of the brand to retrieve item data for.
+   * @returns {Promise<JSON[]>} of brand items data.
    */
-  async fetchAllBrandItemsData(brandId: number): Promise<Dictionary<JSON>> {
-    let allData: JSON[] = [];
-    let i = 1;
-    while (true) {
-      const pageData = await this.fetchBrandItemsData(brandId, i);
-      if (!Array.isArray(pageData) || !pageData.length) {
-        break;
-      }
-      allData = allData.concat(pageData);
-      i++;
-    }
-    return _.keyBy(allData, 'id');
+  public async fetchAllBrandItemsData(brandId: number): Promise<JSON[]> {
+    const BRAND_ITEMS_DATA_RESOURCE = `items/data/brand/${brandId}`;
+
+    return await this.getAllPagedData(BRAND_ITEMS_DATA_RESOURCE);
   }
 
   /**
-   * Fetches brand items data
+   * Fetches all pricing data for a given brand.
    *
-   * @param {number} brandId
-   * @param {number} pageNumber
-   * @returns {Promise<JSON>}
+   * @param {number} brandId the id of the brand to retrieve pricing for.
+   * @returns {Promise<JSON[]>} of brand pricing.
    */
-  async fetchBrandItemsData(
-    brandId: number,
-    pageNumber: number
-  ): Promise<JSON> {
-    const BRAND_ITEMS_RESOURCE = `items/data/brand/${brandId}`;
-    try {
-      const response = await this.axiosClient.get(BRAND_ITEMS_RESOURCE, {
-        params: {
-          page: pageNumber,
-        },
-      });
-      return response.data.data;
-    } catch (e) {
-      if (e.response.status != undefined) {
-        if (e.response.status == 401) {
-          console.error(INVALID_CREDENTIALS);
-          await this.authenticate();
-          await this.fetchBrandItems(brandId, pageNumber);
-        }
-      } else {
-        console.error('🔥 ' + e);
-
-        throw new Turn14Error(
-          'fetchBrandItemsData(), something went wrong communicating with Turn14.'
-        );
-      }
-    }
-
-    return {} as JSON;
-  }
-
-  /**
-   * Fetches all brand pricing as a map of
-   * {'itemId', itemPricing}
-   *
-   * @param {number} brandId
-   * @returns {Promise<Dictionary<JSON>>} of brand pricing
-   */
-  async fetchAllBrandPricing(brandId: number): Promise<Dictionary<JSON>> {
-    let allData: JSON[] = [];
-    let i = 1;
-    while (true) {
-      const pageData = await this.fetchBrandPricing(brandId, i);
-      if (!Array.isArray(pageData) || !pageData.length) {
-        break;
-      }
-      allData = allData.concat(pageData);
-      i++;
-    }
-    return _.keyBy(allData, 'id');
-  }
-
-  /**
-   * Fetches brand pricing
-   *
-   * @param {number} brandId
-   * @param {number} pageNumber
-   * @returns {Promise<JSON>}
-   */
-  async fetchBrandPricing(brandId: number, pageNumber: number): Promise<JSON> {
+  public async fetchAllBrandPricing(brandId: number): Promise<JSON[]> {
     const BRAND_PRICING_RESOURCE = `pricing/brand/${brandId}`;
-    try {
-      const response = await this.axiosClient.get(BRAND_PRICING_RESOURCE, {
-        params: {
-          page: pageNumber,
-        },
-      });
-      return response.data.data;
-    } catch (e) {
-      if (e.response.status != undefined) {
-        if (e.response.status == 401) {
-          console.error(INVALID_CREDENTIALS);
-          await this.authenticate();
-          await this.fetchBrandItems(brandId, pageNumber);
-        }
-      } else {
-        console.error('🔥 ' + e);
 
-        throw new Turn14Error(
-          'fetchBrandPricing(), something went wrong communicating with Turn14.'
-        );
-      }
-    }
-
-    return {} as JSON;
+    return await this.getAllPagedData(BRAND_PRICING_RESOURCE);
   }
 
   /**
-   * Fetches all brand inventory as a map of
-   * {'itemId', itemInventory}
+   * Fetches all inventory for a given brand.
    *
-   * @param {number} brandId
-   * @returns {Promise<Dictionary<JSON>>} of brand inventory
+   * @param {number} brandId the id of the brand to retrieve inventory for.
+   * @returns {Promise<JSON[]>} of brand inventory.
    */
-  async fetchAllBrandInventory(brandId: number): Promise<Dictionary<JSON>> {
+  public async fetchAllBrandInventory(brandId: number): Promise<JSON[]> {
+    const BRAND_INVENTORY_RESOURCE = `inventory/brand/${brandId}`;
+
+    return await this.getAllPagedData(BRAND_INVENTORY_RESOURCE);
+  }
+
+  private async getAllPagedData(resource: string): Promise<JSON[]> {
     let allData: JSON[] = [];
     let i = 1;
     while (true) {
-      const pageData = await this.fetchBrandInventory(brandId, i);
-      if (!Array.isArray(pageData) || !pageData.length) {
+      const pageData = await this.getRequest(resource, i);
+
+      if (this.isDonePaging(pageData)) {
         break;
       }
+
       allData = allData.concat(pageData);
+
       i++;
     }
-    return _.keyBy(allData, 'id');
+
+    return allData;
   }
 
-  /**
-   * Fetches brand inventory
-   *
-   * @param {number} brandId
-   * @param {number} pageNumber
-   * @returns {Promise<JSON>}
-   */
-  async fetchBrandInventory(
-    brandId: number,
+  private async getRequest(resource: string): Promise<JSON[]>;
+
+  private async getRequest(
+    resource: string,
     pageNumber: number
-  ): Promise<JSON> {
-    const BRAND_INVENTORY_RESOURCE = `inventory/brand/${brandId}`;
+  ): Promise<JSON[]>;
+
+  private async getRequest(
+    resource: string,
+    pageNumber?: number
+  ): Promise<JSON[]> {
     try {
-      const response = await this.axiosClient.get(BRAND_INVENTORY_RESOURCE, {
+      const response = await this.get(pageNumber, resource);
+
+      return response.data?.['data'];
+    } catch (e) {
+      throw new Turn14Error(
+        `Something went wrong communicating with Turn14. ${e}`
+      );
+    }
+  }
+
+  private async get(
+    pageNumber: number | undefined,
+    resource: string
+  ): Promise<AxiosResponse<JSON[]>> {
+    let response: AxiosResponse<JSON[]>;
+    if (pageNumber) {
+      response = await this.axiosClient.get(resource, {
         params: {
           page: pageNumber,
         },
       });
-
-      return response.data.data;
-    } catch (e) {
-      if (e.response?.status == 401) {
-        console.error(INVALID_CREDENTIALS);
-
-        await this.authenticate();
-        await this.fetchBrandItems(brandId, pageNumber);
-      } else {
-        console.error('🔥 ' + e);
-
-        throw new Turn14Error(
-          'Something went wrong communicating with Turn14.'
-        );
-      }
+    } else {
+      response = await this.axiosClient.get(resource);
     }
 
-    return {} as JSON;
+    return response;
   }
 
-  /**
-   * Fetches brands
-   *
-   * @returns {Promise<Turn14Brand[]>}
-   */
-  async fetchBrands(): Promise<Turn14Brand[]> {
-    const BRANDS_RESOURCE = `brands`;
-    try {
-      const response = await this.axiosClient.get(BRANDS_RESOURCE);
-      return response.data.data;
-    } catch (e) {
-      if (e.response.status != undefined) {
-        if (e.response.status == 401) {
-          console.error(INVALID_CREDENTIALS);
-          await this.authenticate();
-          await this.fetchBrands();
-        }
-      } else {
-        console.error('🔥 ' + e);
-
-        throw new Turn14Error(
-          'fetchBrands(), something went wrong communicating with Turn14.'
-        );
-      }
-    }
-
-    return [];
+  private isDonePaging(pageData: JSON[]): boolean {
+    return !Array.isArray(pageData) || !pageData.length;
   }
 }
