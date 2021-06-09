1d8b6a65d161a06a6229f1554cf53c2a00d61dfe
= <hallsamuel90@gmail.com>
2021-06-09 02:57:33 +0000

    removed the job scheduling form pmgmt

--- src/productMgmt/config/jobQueueConfig.ts
+++ src/productMgmt/config/jobQueueConfig.ts
@@ -1,14 +1,7 @@
 import Container from 'typedi';
 import { ProductSyncJobPoller } from '../jobQueue/productSyncJobPoller';
-import { ProductSyncJobScheduler } from '../jobQueue/productSyncJobScheduler';
 
 export default (): void => {
   const productSyncJobPoller = Container.get(ProductSyncJobPoller);
   productSyncJobPoller.pollJobQueue();
-
-  const productSyncJobScheduler = Container.get(ProductSyncJobScheduler);
-  productSyncJobScheduler.scheduleInventoryUpdate();
-  productSyncJobScheduler.schedulePricingUpdate();
-  // productSyncJobScheduler.scheduleRemoveStaleProducts();
-  productSyncJobScheduler.scheduleProductResync();
 };
