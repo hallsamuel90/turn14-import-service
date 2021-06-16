2436f267ae40c80e1b1b9c9be068a068d3d35786
= <hallsamuel90@gmail.com>
2021-06-16 01:51:10 +0000

    pr fixes

--- src/jobScheduler.app.ts
+++ src/jobScheduler.app.ts
@@ -1,10 +1,9 @@
 import dotenv from 'dotenv';
 import 'reflect-metadata';
 import expressAppConfig from './config/expressApp';
+import { createQueueIfDoesNotExist } from './config/initQueue';
 import mongoConfig from './config/mongoose';
 import jobSchedulerConfig from './productMgmt/config/jobSchedulerConfig';
-import { ProductSyncQueue } from './productMgmt/jobQueue/models/productSyncQueue';
-import { ProductSyncQueueRepositoryMongo } from './productMgmt/jobQueue/repositories';
 
 // load env variables
 dotenv.config();
@@ -12,17 +11,10 @@ dotenv.config();
 // log ts files instead of js
 require('source-map-support').install();
 
-const queueRepo = new ProductSyncQueueRepositoryMongo();
-
 const appConfig = async (): Promise<void> => {
   await mongoConfig();
 
-  try {
-    await queueRepo.fetchQueue();
-  } catch (e) {
-    console.info('initializing queue...');
-    await queueRepo.save(new ProductSyncQueue());
-  }
+  await createQueueIfDoesNotExist();
 
   await jobSchedulerConfig();
 };
